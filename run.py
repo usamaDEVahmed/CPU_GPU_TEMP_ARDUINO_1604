3675a60f83ceb3746dfe0b5bf7cfc4f47d6ca0ec
usamaDEVahmed <usama.ahmed8565@gmail.com>
2023-08-27 15:00:22 +0500

    added docs to methods

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 import platform
-import time
 from msl.loadlib import LoadLibrary 
-import threading
 
 class Runner():
 
@@ -14,11 +12,11 @@ class Runner():
 
     def __init__(self):
         '''
-        monitor: Reference object to load the data from OpenHardwareMonitorLib.dll
-        cpu_hardware: Reference object set for the first time of execution to make it usable without the need to fetch it fromm .dll again
-        gpu_hardware: Reference object set for the first time of execution to make it usable without the need to fetch it fromm .dll again
-        cpu_temp: Reference object set for the first time of execution to make it usable without the need to fetch it fromm .dll again
-        gpu_temp: Reference object set for the first time of execution to make it usable without the need to fetch it fromm .dll again
+            monitor: Reference object to load the data from OpenHardwareMonitorLib.dll
+            cpu_hardware: Reference object set for the first time of execution to make it usable without the need to fetch it fromm .dll again.
+            gpu_hardware: Reference object set for the first time of execution to make it usable without the need to fetch it fromm .dll again.
+            cpu_temp: Reference object set for the first time of execution to make it usable without the need to fetch it fromm .dll again.
+            gpu_temp: Reference object set for the first time of execution to make it usable without the need to fetch it fromm .dll again.
         '''
         self.monitor = None
         self.cpu_hardware = None
@@ -27,6 +25,11 @@ class Runner():
         self.gpu_temp = None
 
     def get_os(self):
+        '''
+            Returns what OS this script is currently running on.
+            If OS is WINDOWS then libs/OpenHardwareMonitorLib.dll will be used to get the CPU and GPU stats.
+            TODO: DOCS Update for LINUX
+        '''
         os = platform.system()
         if os.lower() == Runner.WINDOWS.lower():
             return Runner.WINDOWS
@@ -36,6 +39,9 @@ class Runner():
             return Runner.OS_NOT_FOUND
         
     def initialize_openhardwaremonitor(self):
+        '''
+            Fetches the Hardware reference from OpenHardwareMonitorLib.dll and decompiles it from 32-bit to 64-bit to be embedded in Python version 3.8.10 x64.
+        '''
         open_hardware_monitor_lib = LoadLibrary('libs\\OpenHardwareMonitorLib.dll', 'net')
         openHardwareMonitor = open_hardware_monitor_lib.lib.OpenHardwareMonitor.Hardware
         monitor = openHardwareMonitor.Computer()
@@ -46,6 +52,9 @@ class Runner():
         self.monitor = monitor
 
     def set_temperature_sensors_from_dll(self, monitor):
+        '''
+            Fetches the sensors references from OpenHardwareMonitorLib.dll for CPU and GPU and sets them at object level.
+        '''
         for hardware in monitor.Hardware:
             for sensor in hardware.Sensors:
                 if str(sensor.SensorType).lower() == 'temperature':
@@ -57,8 +66,9 @@ class Runner():
                         self.gpu_temp = sensor
     
     def get_stats_win(self):
-        # monitor reference to refer the OpenhardwareMonitorLib.dll is only set for first time of execution.
-        # Other times, the set reference is used only.
+        '''
+            Returns the stats by initializing the relevant objects and using them to get the data.
+        '''
         if self.monitor == None:
             self.initialize_openhardwaremonitor()
         
@@ -74,6 +84,9 @@ class Runner():
         return 'linux'
 
     def get_stats(self):
+        '''
+            Common method that uses the methods depending upon the OS and returns the stats
+        '''
         current_os = self.get_os()
         if current_os == Runner.WINDOWS:
             return self.get_stats_win()
