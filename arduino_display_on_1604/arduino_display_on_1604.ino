cce7383701ce59825517ee3c298a8c85d4ef85e0
usamaDEVahmed <usama.ahmed8565@gmail.com>
2023-09-02 20:55:44 +0500

    added file for arduino to read the data via Serial Communication and display it on serial console

---
+++ arduino_display_on_1604/arduino_display_on_1604.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.setTimeout(1);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (!Serial.available())
+  {
+    String stats = Serial.readString();
+    Serial.print(stats);
+  }
+}
