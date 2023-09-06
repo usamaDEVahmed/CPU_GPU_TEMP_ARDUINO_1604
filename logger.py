0d19f7714378351a2469cdc233df4c0c7a8dac19
usamaDEVahmed <usama.ahmed8565@gmail.com>
2023-09-07 01:37:17 +0500

    changed logger config file to .ini file and configured logger to output in file as well as console

--- logger.py
+++ logger.py
@@ -1,10 +1,17 @@
 import logging
+import os
 import logging.config
 
 
 class Logger():
         
+    LOG_DIR = 'logs'
+
     @staticmethod
     def get_logger(name):
-        logging.config.fileConfig('log_config.json')
+        
+        if not os.path.exists(Logger.LOG_DIR):
+            os.mkdir(Logger.LOG_DIR)
+
+        logging.config.fileConfig('log_config.ini')
         return logging.getLogger(name)
